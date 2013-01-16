81ba6eebff33b76b538a022cf15a0136b71fbca3
pupunzi <matteo@open-lab.com>
2013-01-16 22:59:53 +0100

    version:"1.1" Update for jQuery 1.9 compatibility

--- inc/jquery.mb.disclose.js
+++ inc/jquery.mb.disclose.js
@@ -14,15 +14,20 @@
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
  *
- *  last modified: 04/01/13 17.13
+ *  last modified: 16/01/13 22.55
  *  *****************************************************************************
  */
+
+/*Browser detection patch*/
+(function(){if(!(8>jQuery.fn.jquery.split(".")[1])){jQuery.browser={};jQuery.browser.mozilla=!1;jQuery.browser.webkit=!1;jQuery.browser.opera=!1;jQuery.browser.msie=!1;var a=navigator.userAgent;jQuery.browser.name=navigator.appName;jQuery.browser.fullVersion=""+parseFloat(navigator.appVersion);jQuery.browser.majorVersion=parseInt(navigator.appVersion,10);var c,b;if(-1!=(b=a.indexOf("Opera"))){if(jQuery.browser.opera=!0,jQuery.browser.name="Opera",jQuery.browser.fullVersion=a.substring(b+6),-1!=(b= a.indexOf("Version")))jQuery.browser.fullVersion=a.substring(b+8)}else if(-1!=(b=a.indexOf("MSIE")))jQuery.browser.msie=!0,jQuery.browser.name="Microsoft Internet Explorer",jQuery.browser.fullVersion=a.substring(b+5);else if(-1!=(b=a.indexOf("Chrome")))jQuery.browser.webkit=!0,jQuery.browser.name="Chrome",jQuery.browser.fullVersion=a.substring(b+7);else if(-1!=(b=a.indexOf("Safari"))){if(jQuery.browser.webkit=!0,jQuery.browser.name="Safari",jQuery.browser.fullVersion=a.substring(b+7),-1!=(b=a.indexOf("Version")))jQuery.browser.fullVersion= a.substring(b+8)}else if(-1!=(b=a.indexOf("Firefox")))jQuery.browser.mozilla=!0,jQuery.browser.name="Firefox",jQuery.browser.fullVersion=a.substring(b+8);else if((c=a.lastIndexOf(" ")+1)<(b=a.lastIndexOf("/")))jQuery.browser.name=a.substring(c,b),jQuery.browser.fullVersion=a.substring(b+1),jQuery.browser.name.toLowerCase()==jQuery.browser.name.toUpperCase()&&(jQuery.browser.name=navigator.appName);if(-1!=(a=jQuery.browser.fullVersion.indexOf(";")))jQuery.browser.fullVersion=jQuery.browser.fullVersion.substring(0, a);if(-1!=(a=jQuery.browser.fullVersion.indexOf(" ")))jQuery.browser.fullVersion=jQuery.browser.fullVersion.substring(0,a);jQuery.browser.majorVersion=parseInt(""+jQuery.browser.fullVersion,10);isNaN(jQuery.browser.majorVersion)&&(jQuery.browser.fullVersion=""+parseFloat(navigator.appVersion),jQuery.browser.majorVersion=parseInt(navigator.appVersion,10));jQuery.browser.version=jQuery.browser.majorVersion}})(jQuery);
 
 /*******************************************************************************
  * jQuery.mb.components: jquery.mb.CSSAnimate
  ******************************************************************************/
 
-$.fn.CSSAnimate=function(a,c,h,i,j,e){return this.each(function(){var d=$(this);if(0!==d.length&&a){"function"==typeof c&&(e=c);"function"==typeof h&&(e=h);"function"==typeof i&&(e=i);"function"==typeof j&&(e=j);if("string"==typeof c)for(var k in $.fx.speeds)if(c==k){c=$.fx.speeds[k];break}else c=null;c||(c=$.fx.speeds._default);i||(i="cubic-bezier(0.65,0.03,0.36,0.72)");j||(j="all");h||(h=0);if(jQuery.support.transition){var b="",f="transitionEnd";$.browser.webkit?(b="-webkit-",f="webkitTransitionEnd"): $.browser.mozilla?(b="-moz-",f="transitionend"):$.browser.opera?(b="-o-",f="oTransitionEnd"):$.browser.msie&&(b="-ms-",f="msTransitionEnd");for(var g in a)"transform"===g&&(a[b+"transform"]=a[g],delete a[g]),"transform-origin"===g&&(a[b+"transform-origin"]=a[g],delete a[g]);d.css(b+"transition-property",j);d.css(b+"transition-duration",c+"ms");d.css(b+"transition-delay",h+"ms");d.css(b+"transition-timing-function",i);setTimeout(function(){d.css(a)},1);var l=function(){d.get(0).removeEventListener(f, l,!1);d.css(b+"transition","");"function"==typeof e&&e()};d.get(0).addEventListener(f,l,!1)}else d.animate(a,c,e)}})};$.fn.CSSAnimateStop=function(){var a="";$.browser.webkit?a="-webkit-":$.browser.mozilla?a="-moz-":$.browser.opera?a="-o-":$.browser.msie&&(a="-ms-");$(this).css(a+"transition","")}; $.support.transition=function(){var a=(document.body||document.documentElement).style;return void 0!==a.transition||void 0!==a.WebkitTransition||void 0!==a.MozTransition||void 0!==a.MsTransition||void 0!==a.OTransition}();
+jQuery.fn.CSSAnimate=function(a,b,k,l,f){return this.each(function(){var c=jQuery(this);if(0!==c.length&&a){"function"==typeof b&&(f=b,b=jQuery.fx.speeds._default);"function"==typeof k&&(f=k,k=0);"function"==typeof l&&(f=l,l="cubic-bezier(0.65,0.03,0.36,0.72)");if("string"==typeof b)for(var j in jQuery.fx.speeds)if(b==j){b=jQuery.fx.speeds[j];break}else b=null;if(jQuery.support.transition){var e="",h="transitionEnd";jQuery.browser.webkit?(e="-webkit-",h="webkitTransitionEnd"):jQuery.browser.mozilla? (e="-moz-",h="transitionend"):jQuery.browser.opera?(e="-o-",h="otransitionend"):jQuery.browser.msie&&(e="-ms-",h="msTransitionEnd");j=[];for(d in a){var g=d;"transform"===g&&(g=e+"transform",a[g]=a[d],delete a[d]);"transform-origin"===g&&(g=e+"transform-origin",a[g]=a[d],delete a[d]);j.push(g);c.css(g)||c.css(g,0)}d=j.join(",");c.css(e+"transition-property",d);c.css(e+"transition-duration",b+"ms");c.css(e+"transition-delay",k+"ms");c.css(e+"transition-timing-function",l);c.css(e+"backface-visibility", "hidden");setTimeout(function(){c.css(a)},0);setTimeout(function(){c.called||!f?c.called=!1:f()},b+20);c.on(h,function(a){c.off(h);c.css(e+"transition","");a.stopPropagation();"function"==typeof f&&(c.called=!0,f());return!1})}else{for(var d in a)"transform"===d&&delete a[d],"transform-origin"===d&&delete a[d],"auto"===a[d]&&delete a[d];if(!f||"string"===typeof f)f="linear";c.animate(a,b,f)}}})}; jQuery.fn.CSSAnimateStop=function(){var a="",b="transitionEnd";jQuery.browser.webkit?(a="-webkit-",b="webkitTransitionEnd"):jQuery.browser.mozilla?(a="-moz-",b="transitionend"):jQuery.browser.opera?(a="-o-",b="otransitionend"):jQuery.browser.msie&&(a="-ms-",b="msTransitionEnd");jQuery(this).css(a+"transition","");jQuery(this).off(b)}; jQuery.support.transition=function(){var a=(document.body||document.documentElement).style;return void 0!==a.transition||void 0!==a.WebkitTransition||void 0!==a.MozTransition||void 0!==a.MsTransition||void 0!==a.OTransition}();
+
+
 
 /*******************************************************************************
  DATA:
@@ -51,7 +56,7 @@ $.fn.CSSAnimate=function(a,c,h,i,j,e){return this.each(function(){var d=$(this);
   $.disclose = {
     name:"mb.disclose",
     author:"Matteo Bicocchi",
-    version:"1.0",
+    version:"1.1",
     defaults:{
       slideIntervall:5000,
       inTimer:600,
@@ -169,7 +174,6 @@ $.fn.CSSAnimate=function(a,c,h,i,j,e){return this.each(function(){var d=$(this);
       banner.show();
 
       var fn= banner.children().eq(0).data("onenter") ? eval("("+banner.children().eq(0).data("onenter")+")") : el.opt.onEnter;
-      console.debug(banner.children().eq(0).data("onenter"))
       if(typeof fn == "function")
         fn(el);
 
@@ -300,7 +304,7 @@ $.fn.CSSAnimate=function(a,c,h,i,j,e){return this.each(function(){var d=$(this);
         fn(el);
 
       setTimeout(function(){
-        $el.CSSAnimate($elAnim, el.opt.outTimer, null, el.opt.ease, "all", function(){
+        $el.CSSAnimate($elAnim, el.opt.outTimer, null, el.opt.ease, function(){
           $el.remove();
           el.canAnimate = true;
         });
@@ -313,7 +317,7 @@ $.fn.CSSAnimate=function(a,c,h,i,j,e){return this.each(function(){var d=$(this);
       /*ENTER*/
       el.actualBanner = $newEl;
       setTimeout(function(){
-        el.actualBanner.CSSAnimate({top:0, left:0, opacity:1, transform: "rotate(0deg) scale(1)" }, el.opt.inTimer,null, el.opt.ease, "all", function(){
+        el.actualBanner.CSSAnimate({top:0, left:0, opacity:1, transform: "rotate(0deg) scale(1)" }, el.opt.inTimer,null, el.opt.ease, function(){
           el.actualBanner.children().eq(0).addClass("in");
           var fn= $newElProp.data("onenter") ? eval("("+$newElProp.data("onenter")+")") : el.opt.onEnter;
           if(typeof fn == "function")
@@ -359,7 +363,7 @@ $.fn.CSSAnimate=function(a,c,h,i,j,e){return this.each(function(){var d=$(this);
 
         $el.css(cssStart);
         setTimeout(function(){
-          $el.CSSAnimate(cssEnd, time, cssDelay, el.opt.ease, "all", function(){});
+          $el.CSSAnimate(cssEnd, time, cssDelay, el.opt.ease, function(){});
         },100);
       });
     },
